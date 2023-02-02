d5533f36ec45189caaa995fd78b856d151e583a3
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-02-02 13:34:31 +0330

    editV03

--- littleProject01_NJS/view/app/src/pages/profile/index.jsx
+++ littleProject01_NJS/view/app/src/pages/profile/index.jsx
@@ -1,35 +1,28 @@
-import testProfile from '../../assets/img/first-icon.jpg'
-import companyLogo from '../../assets/img/company-logo.png'
-import './index.css'
+import testProfile from "../../assets/img/first-icon.jpg";
+import companyLogo from "../../assets/img/company-logo.png";
+import "./index.css";
 
-export  function Profile() {
-    return(
-        <div className='display-flex gap-155 justify-center margin-top-155'>
-            <div className="icon-profile-container display-flex items-center flex-direction-col">
-                <img className='user-profile' src={testProfile} alt="firstIcon" />
-                <img className='company-main-logo border-radius-19' src={companyLogo} alt="company logo" />
-            </div>
-            <div className='info-text-container'>
-                <div>
-                    PHIL Janet anderson
-                </div>
-                <div>
-                    Sales Representative
-                </div>
-                <div>
-                    +123-456-789
-                </div>
-                <div>
-                    Hello@reallygreatsite.com
-                </div>
-                <div>
-                    www.reallygreatsite.com
-                </div>
-                <div>
-                    123 AnyWhere st., Any City, st 1234
-                </div>
-            </div>
+export function Profile() {
+  return (
+    <div className="display-flex gap-155 justify-center margin-top-155">
+      <div className="icon-profile-container display-flex items-center flex-direction-col">
+        <img className="user-profile " src={testProfile} alt="firstIcon" />
+        <img
+          className="company-main-logo border-radius-19"
+          src={companyLogo}
+          alt="company logo"
+        />
+      </div>
+      <div className="info-text-container display-flex items-center flex-direction-col">
+        <div className="font-size-32 font-weight-bold padding-bot-16 ">PHIL Janet anderson</div>
+        <div className="font-size-22 font-weight-400 padding-bot-32">Sales Representative</div>
+        <div className="more-info-container display-flex flex-direction-col">
+          <div className="padding-bot-16 padding-top-16 border-bot-1px-black">+123-456-789</div>
+          <div className="padding-bot-16 padding-top-16 border-bot-1px-black">Hello@reallygreatsite.com</div>
+          <div className="padding-bot-16 padding-top-16 border-bot-1px-black">www.reallygreatsite.com</div>
+          <div className="padding-top-16">123 AnyWhere st., Any City, st 1234</div>
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
