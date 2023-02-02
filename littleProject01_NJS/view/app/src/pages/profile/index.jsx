36bb4b8b2da83e35e220fbd606a1eced20eea3e4
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-02-02 21:56:44 +0330

    editV06

--- littleProject01_NJS/view/app/src/pages/profile/index.jsx
+++ littleProject01_NJS/view/app/src/pages/profile/index.jsx
@@ -1,23 +1,84 @@
 import testProfile from "../../assets/img/first-icon.jpg";
+import test02Profile from "../../assets/img/second-icon.jpg";
 import companyLogo from "../../assets/img/company-logo.png";
+import {  Radio } from "pretty-checkbox-react";
+import { HexColorPicker } from "react-colorful";
+import "@djthoms/pretty-checkbox";
+import { useState } from "react";
 import "./index.css";
 
+
 export function Profile() {
+  const [color, setColor] = useState("#000000");
+  const [state , setState] = useState(false)
+  const [ifSaved , setSave] = useState(true)
   return (
     <div className="height-100vh over-flow-hidden">
-      <div className="change-info-container">
-      <div className="change-info-inputs-container display-flex flex-direction-col">
-        <div>Choose Your Prefer</div>
-        <div>gg</div>
-        <button>gg</button>
-        <input placeholder="username" type='text'/>
-        <input placeholder="number" type='text'/>
-        <input placeholder="email" type='text'/>
-        <input placeholder="website" type='text'/>
-        <input placeholder="addrese" type='text'/>
-        <figure>gg</figure>
-        <button className="pink-button" onClick={()=>{console.log('clicked')}}>Save</button>
-      </div>
+      <div className="change-info-container" style={{top: ifSaved?'-48rem':'0rem' , transition:'all .6s'}} onClick={(e)=>{
+        if(e.target.className === 'change-info-container'){e.target.style.top = '-48rem'}
+        }}>
+        <div className="change-info-inputs-container display-flex flex-direction-col items-start gap-16">
+          <div>Choose Your Prefer</div>
+          <div>gg</div>
+          <div className="hex-color-changer " style={{display:state? 'flex' : 'none'}}>
+            <HexColorPicker color={color}  onChange={setColor} />
+          </div>
+          <button  style={{background: color,padding:'1rem'}} onClick={()=>{
+            setState(!state)
+            console.log(state)
+            }}/>
+          <input
+            className="width-100-percent padd-8"
+            placeholder="username"
+            type="text"
+          />
+          <input
+            className="width-100-percent padd-8"
+            placeholder="number"
+            type="text"
+          />
+          <input
+            className="width-100-percent padd-8"
+            placeholder="email"
+            type="text"
+          />
+          <input
+            className="width-100-percent padd-8"
+            placeholder="website"
+            type="text"
+          />
+          <input
+            className="width-100-percent padd-8"
+            placeholder="addrese"
+            type="text"
+          />
+          <div className="display-flex">
+            <figure>
+              <img
+                className="first-profile z-index-6"
+                src={testProfile}
+                alt="firstIcon"
+              />
+              <Radio name="a" shape="round" color="primary" />
+
+            </figure>
+            <figure>
+              <img
+                className="first-profile z-index-6"
+                src={test02Profile}
+                alt="firstIcon"
+              />
+              <Radio name="a" shape="round" color="primary" />
+            </figure>
+          </div>
+
+          <button
+            className="pink-button width-100-percent"
+            onClick={()=>{setSave(!ifSaved)}}
+          >
+            Save
+          </button>
+        </div>
       </div>
       <div className="box-test">
         <div className="box-test02">
@@ -57,7 +118,7 @@ export function Profile() {
                   </div>
                 </div>
               </div>
-              <button className="pink-button custom-class-button-profile-page z-index-6">
+              <button className="pink-button custom-class-button-profile-page z-index-6" onClick={()=>{setSave(!ifSaved)}} >
                 My Inputs
               </button>
             </div>
